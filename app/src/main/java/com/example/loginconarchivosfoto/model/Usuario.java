fb876d901d41e2041a5fb5a23099454994293a08
KevinPa26 <kevinenriquep26@gmail.com>
2022-05-12 16:12:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/loginconarchivosfoto/model/Usuario.java
@@ -0,0 +1,73 @@
+package com.example.loginconarchivosfoto.model;
+
+import android.graphics.Bitmap;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private long dni;
+    private String apellido;
+    private String nombre;
+    private String mail;
+    private String password;
+    private byte [] b;
+
+    public Usuario(long dni, String apellido, String nombre, String mail, String password, Bitmap imagenBit) {
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.mail = mail;
+        this.password = password;
+        this.b = imagenBit;
+    }
+
+    public Usuario(){}
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Bitmap getImagenBit() {
+        return imagenBit;
+    }
+
+    public void setImagenBit(Bitmap imagenBit) {
+        this.imagenBit = imagenBit;
+    }
+}
